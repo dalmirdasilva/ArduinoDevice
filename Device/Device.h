cf890620bf180303b903a545abe2a4321293fe24
Dalmir Silva <dalmirdasilva@gmail.com>
2015-08-06 18:35:42 -0300

    first commit

---
+++ Device/Device.h
@@ -0,0 +1,24 @@
+/**
+ * Arduino - Device
+ *
+ * Device.cpp
+ *
+ * @author Dalmir da Silva <dalmirdasilva@gmail.com>
+ */
+
+#ifndef __ARDUINO_DRIVER_DEVICE_H__
+#define __ARDUINO_DRIVER_DEVICE_H__ 1
+
+class Device {
+    
+public:
+    
+    /**
+     * Public constructor
+     * 
+     * @param address       The wire address.
+     */
+     Device();
+ };
+
+#endif /* __ARDUINO_DRIVER_DEVICE_H__ */
