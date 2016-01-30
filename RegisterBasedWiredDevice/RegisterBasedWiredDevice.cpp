34c6be87b050bb1b52ba5672b7a2c5de2f27615b
Dalmir Silva <dalmirdasilva@gmail.com>
2016-01-30 02:02:24 -0200

    added read/write in block for device

--- RegisterBasedWiredDevice/RegisterBasedWiredDevice.cpp
+++ RegisterBasedWiredDevice/RegisterBasedWiredDevice.cpp
@@ -2,13 +2,11 @@
 #include <Wire.h>
 #include "RegisterBasedWiredDevice.h"
 
-RegisterBasedWiredDevice::RegisterBasedWiredDevice(
-        unsigned char address) :
-        WiredDevice(address) {
+RegisterBasedWiredDevice::RegisterBasedWiredDevice(unsigned char address)
+        : WiredDevice(address) {
 }
 
-void RegisterBasedWiredDevice::configureRegisterBits(unsigned char reg,
-        unsigned char mask, unsigned char d) {
+void RegisterBasedWiredDevice::configureRegisterBits(unsigned char reg, unsigned char mask, unsigned char d) {
     unsigned char n;
     n = readRegister(reg);
     n &= ~(mask);
@@ -16,28 +14,44 @@ void RegisterBasedWiredDevice::configureRegisterBits(unsigned char reg,
     writeRegister(reg, n);
 }
 
-unsigned char RegisterBasedWiredDevice::writeRegister(unsigned char reg,
-        unsigned char d) {
+unsigned char RegisterBasedWiredDevice::writeRegisterBlock(unsigned char reg, unsigned char *buff, unsigned char len) {
     Wire.beginTransmission(getDeviceAddress());
     Wire.write(reg);
-    Wire.write(d);
+    Wire.write(buff, len);
     return Wire.endTransmission();
 }
 
-int RegisterBasedWiredDevice::readRegister(unsigned char reg) {
+int RegisterBasedWiredDevice::readRegisterBlock(unsigned char reg, unsigned char *buf, unsigned char len) {
     char tries = MAX_RETRIES_ON_READING;
+    unsigned char i;
     Wire.beginTransmission(getDeviceAddress());
     Wire.write(reg);
     char status = Wire.endTransmission(false);
     if (status != 0) {
         return -(status);
     }
-    Wire.requestFrom(getDeviceAddress(), (unsigned char) 1);
+    Wire.requestFrom(getDeviceAddress(), len);
     while (!Wire.available() && --tries > 0) {
         delayMicroseconds(1);
     }
     if (tries == 0) {
         return -5;
     }
-    return Wire.read();
+    for (i = 0; i < len && Wire.available(); i++) {
+        buf[i] = Wire.read();
+    }
+    return i;
+}
+
+unsigned char RegisterBasedWiredDevice::writeRegister(unsigned char reg, unsigned char d) {
+    return writeRegisterBlock(reg, &d, 1);
+}
+
+int RegisterBasedWiredDevice::readRegister(unsigned char reg) {
+    unsigned char c;
+    int r = readRegisterBlock(reg, &c, 1);
+    if (r < 0) {
+        return r;
+    }
+    return (int) c;
 }
--- RegisterBasedWiredDevice/RegisterBasedWiredDevice.h
+++ RegisterBasedWiredDevice/RegisterBasedWiredDevice.h
@@ -49,6 +49,34 @@ public:
      * @return              The register value.
      */
     int readRegister(unsigned char reg);
+
+    /**
+     * Reads values from the device, starting by the reg register.
+     *
+     * @param reg           The register number.
+     * @param buf           The buffer where to place read bytes.
+     * @param len           How many bytes to read.
+     * @return              If >= 0: How many bytes were read.
+     *                      If < 0: Error code:
+     *                      <ul>
+     *                          <li>-1: data too long to fit in transmit buffer</li>
+     *                          <li>-2: received NACK on transmit of address</li>
+     *                          <li>-3: received NACK on transmit of data</li>
+     *                          <li>-4: other error</li>
+     *                          <li>-5: timeout</li>
+     *                      </ul>
+     */
+    int readRegisterBlock(unsigned char reg, unsigned char *buf, unsigned char len);
+
+    /**
+     * Writes a sequence of values to a sequence of registers, starting by the reg address.
+     *
+     * @param reg           The register number.
+     * @param buf           The buffer.
+     * @param len           Buffer length.
+     * @return              The result of Wire.endTransmission().
+     */
+    unsigned char writeRegisterBlock(unsigned char reg, unsigned char *buf, unsigned char len);
 };
 
 #endif /* __ARDUINO_DRIVER_REGISTER_BASED_WIRED_DEVICE_H__ */
